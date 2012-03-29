0b6c321f1862287348ed486c46043071b28712bf
Jacob Mather <jmather@jmather.com>
2012-03-29 10:23:53 -0400

    Fixed a minor issue with how a variable was named from twilio. Not sure if the docs are wrong or I made a mistake.

--- lib/Majax/Twilio/Client/Status/TextResponse.php
+++ lib/Majax/Twilio/Client/Status/TextResponse.php
@@ -17,7 +17,8 @@ class Majax_Twilio_Client_Status_TextResponse
     private $account_sid;
     private $status;
     private $body;
-    private $sms_id;
+    private $sms_message_id;
+    private $sms_sid;
     private $to;
     private $from;
     private $api_version;
@@ -27,7 +28,8 @@ class Majax_Twilio_Client_Status_TextResponse
         $this->account_sid = $post_vars['AccountSid'];
         $this->status = $post_vars['SmsStatus'];
         $this->body = $post_vars['Body'];
-        $this->sms_id = $post_vars['SmsId'];
+        $this->sms_message_id = $post_vars['SmsMessageSid'];
+        $this->sms_sid = $post_vars['SmsSid'];
         $this->to = $post_vars['To'];
         $this->from = $post_vars['From'];
         $this->api_version = $post_vars['ApiVersion'];
@@ -53,11 +55,6 @@ class Majax_Twilio_Client_Status_TextResponse
         return $this->from;
     }
 
-    public function getSmsId()
-    {
-        return $this->sms_id;
-    }
-
     public function getStatus()
     {
         return $this->status;
@@ -74,4 +71,14 @@ class Majax_Twilio_Client_Status_TextResponse
             return true;
         return false;
     }
+
+  public function getSmsMessageId()
+  {
+    return $this->sms_message_id;
+  }
+
+  public function getSmsSid()
+  {
+    return $this->sms_sid;
+  }
 }
